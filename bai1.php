a9eea0e9e64fd2cb46e9620956b999fb392140dc
thongnv <thongnv@smartosc.com>
2020-09-10 09:28:34 +0700

    update file bai1.php

--- bai1.php
+++ bai1.php
@@ -14,20 +14,25 @@ class MyCircle {
    }
  }
  // chuyen string sang float
- $Convert=array_map('floatval',$argv)[1];
-
- if(is_numeric($Convert) && $Convert>=0
+ //$Convert=array_map('floatval',$argv)[1];
+ // Check if the user has passed the command name?
+if(isset( $argv[1])){
+ if(is_numeric($argv[1]) && $argv[1]>=0
  ){
   $cirle=new MyCircle();
-  echo "Dien tich hinh tron: {$cirle->getArea($Convert)}\n";
-  echo"Chu vi hinh tron: {$cirle->perimeter($Convert)}\n";
+  echo "Dien tich hinh tron: {$cirle->getArea($argv[1])}\n";
+  echo"Chu vi hinh tron: {$cirle->perimeter($argv[1])}\n";
 }
-else if(is_numeric($Convert) && $Convert<0 ){
+else if(is_numeric($argv[1]) && $argv[1]<0 ){
   echo "Ban kinh khong duoc am.Nhap lai ban kinh nhe!\n";
 
 }
+else{
+  echo "Ban kinh phai la so!!!Moi ban nhap lai!\n";
+}
+}
  else{
-  echo "Nhap lai ban kinh nhe!\n";
+  echo "Ban kinh khong the null!\n";
 
   }
 ?>
